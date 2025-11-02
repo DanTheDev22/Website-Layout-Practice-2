c65711aa9bad8643709bba8292de3122df16d891
DanTheDev <daniel1mogoreanu@gmail.com>
2025-11-02 17:03:19 +0200

    done the entire styling, TO DO Responsiveness

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,35 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { 
-      js ,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
       prettier: prettierPlugin,
-
-    }, 
-    extends: [
-      "js/recommended"
-    ], 
-    languageOptions: { 
-      globals: globals.browser 
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
     },
     rules: {
       ...prettierPlugin.configs.recommended.rules,
-      'no-invalid-regexp':'warn',
-      'no-irregular-whitespace':'warn',
-      'no-undef':'warn',
-      'no-unreachable':'warn',
-      'no-unused-vars':'warn',
-      'require-atomic-updates':'warn',
-      'camelcase': 'warn',
-      'eqeqeq':['error', 'always'],  
-      'no-unused-expressions':'warn',
-      'no-useless-catch':'warn',
-      'no-self-assign':'warn',
+      'no-invalid-regexp': 'warn',
+      'no-irregular-whitespace': 'warn',
+      'no-undef': 'warn',
+      'no-unreachable': 'warn',
+      'no-unused-vars': 'warn',
+      'require-atomic-updates': 'warn',
+      camelcase: 'warn',
+      eqeqeq: ['error', 'always'],
+      'no-unused-expressions': 'warn',
+      'no-useless-catch': 'warn',
+      'no-self-assign': 'warn',
       'no-console': 'warn',
-      'curly': 'error'
-    }
-  }
+      curly: 'error',
+    },
+  },
 ]);
